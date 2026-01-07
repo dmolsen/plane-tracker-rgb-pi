f14b5504bfaaf2c082a9caee870e4317b0059bf7
Dave Olsen <dmolsen@mail.wvu.edu>
2026-01-07 07:25:24 -0500

    debugging

--- its-a-plane-python/scenes/smoke.py
+++ its-a-plane-python/scenes/smoke.py
@@ -7,15 +7,5 @@ class SmokeScene(object):
 
     @Animator.KeyFrame.add(1,tag="smoke")
     def smoke(self, count):
-        # erase previous pixel
-        if self._last is not None:
-            lx, ly = self._last
-            self.canvas.SetPixel(lx, ly, 0, 0, 0)
-
-        # draw new pixel
-        x = count % 64
-        y = count % 32
-        self.canvas.SetPixel(x, y, 255, 255, 255)
-
-        self._last = (x, y)
+        self.canvas.SetPixel(22, 22, 255, 255, 255)
         return False

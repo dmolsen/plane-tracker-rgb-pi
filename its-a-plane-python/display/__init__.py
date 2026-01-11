b2fdbe1e869050646ae05a15bcad81e134df57cf
Dave Olsen <dmolsen@mail.wvu.edu>
2026-01-11 10:29:47 -0500

    forcing removal of other scenes

--- its-a-plane-python/display/__init__.py
+++ its-a-plane-python/display/__init__.py
@@ -115,15 +115,15 @@ def desired_brightness():
 
 class Display(
     # “Home” widgets:
-    #TemperatureScene,
-    #ClockScene,
-    #DateScene,
-    #DaysForecastScene,
+    TemperatureScene,
+    ClockScene,
+    DateScene,
+    DaysForecastScene,
 
     # “Flight” widgets:
-    #FlightLogoScene,
-    #JourneyScene,
-    FlightDetailsScene,
+    FlightLogoScene,
+    JourneyScene,
+    #FlightDetailsScene,
     PlaneDetailsScene,
 
     # status widget:

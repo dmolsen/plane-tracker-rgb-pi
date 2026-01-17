38c461d6d67c367a1bd493afe644a0d993b99a99
Dave Olsen <dmolsen@mail.wvu.edu>
2026-01-17 14:11:40 -0500

    updating our flight background so other things will draw

--- its-a-plane-python/scenes/flightbackground.py
+++ its-a-plane-python/scenes/flightbackground.py
@@ -7,7 +7,9 @@ class FlightBackgroundScene(object):
     def __init__(self):
         super().__init__()
 
-    @Animator.KeyFrame.add(1, tag="flight_bg")
+    @Animator.KeyFrame.add(1, tag="flight_bg", order=0)
     def flight_background(self, count):
         # Full-screen black background for flight mode.
         self.draw_square(0, 0, screen.WIDTH, screen.HEIGHT, colours.BLACK)
+        # Signal other flight scenes to redraw this frame.
+        self._redraw_all_this_frame = True
